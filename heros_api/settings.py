c8d9509da28a86f121471b86e8627829650c7727
Elias <eliasful@gmail.com>
2018-11-29 21:42:59 -0200

    configuracao heroku

--- heros_api/settings.py
+++ heros_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import decouple from config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k@q0xc#kj#uk)1zev$_o#pvj85rk6d(kltkzfqxja*i_2lh65o'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['heros-api.herokuapp.com', 'localhost:8000']
 
 
 # Application definition
@@ -78,13 +79,10 @@ WSGI_APPLICATION = 'heros_api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+from dj_database_url import parse as dburl
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 
 # Password validation
@@ -132,3 +130,5 @@ MEDIA_URL = '/media/'
 CORS_ORIGIN_WHITELIST = (
     'localhost:4200',
 )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
