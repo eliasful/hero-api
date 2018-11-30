cdcf50aa69200780087c0187aa3743ca97e3d4c8
Elias <elias@gitlab.viasoft.com.br:8288>
2018-11-30 09:43:31 -0200

    altera tipo da imagem

--- hero/models.py
+++ hero/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Hero(models.Model):
     name = models.CharField(max_length=40)
     description = models.TextField()
-    photo = models.ImageField(upload_to='photos', null=True, blank=True)
+    photo = models.TextField(blank=True, null=True)
     favorite = models.BooleanField(default=False)
-    
-    
+
+
     def __str__(self):
         return self.name
--- heros_api/settings.py
+++ heros_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k@q0xc#kj#uk)1zev$_o#pvj85rk6d(kltkzfqxja*i_2lh65o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []  
 
 
 # Application definition
